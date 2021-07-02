421ad90a88917b0e2d329f7e3ca9f44fb80e0e53
Kitt Phi <46500659+KittPhi@users.noreply.github.com>
2021-07-02 00:31:40 -0600

    Delete vonage_adapter.js
    delete test file

--- vonage_adapter.js
+++
@@ -1,7 +0,0 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
-
-function otherMsg() {
-  console.log("This is another message from the demo package");
-}
